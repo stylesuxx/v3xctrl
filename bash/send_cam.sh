2c5d8ff8e8cde6aa6a6fa6793965a1372f8d5a7a
Chris L <stylesuxx@gmail.com>
2025-03-21 22:49:57 +0100

    Read parameters for GST script from config file

--- bash/send_cam.sh
+++ bash/send_cam.sh
@@ -1,33 +1,59 @@
 #! /bin/bash
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 HOST PORT"
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 HOST PORT [--width val] [--height val] [--framerate val] [--bitrate val]"
     exit 1
 fi
 
 HOST="$1"
 PORT="$2"
+shift 2
 
 # NOTE: Eher laggy
-BITRATE=5000000
-WIDTH=1920
-HEIGHT=1080
-FRAMERATE=30/1
+# BITRATE=5000000
+# WIDTH=1920
+# HEIGHT=1080
+# FRAMERATE=30
 
 # NOTE: Nahezu perfekt
 WIDTH=1280
 HEIGHT=720
 BITRATE=3000000
-FRAMERATE=30/1
+FRAMERATE=30
 CAMBUFFERTIME=50000000
 
 # NOTE: Sehr matschig - muesste vermutlich mehr bitrate haben.
 # WIDTH=1280
 # HEIGHT=720
 # BITRATE=1500000
-# FRAMERATE=60/1
+# FRAMERATE=60
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --width)
+      WIDTH="$2"
+      shift 2
+      ;;
+    --height)
+      HEIGHT="$2"
+      shift 2
+      ;;
+    --framerate)
+      FRAMERATE="$2"
+      shift 2
+      ;;
+    --bitrate)
+      BITRATE="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
 
 gst-launch-1.0 -v libcamerasrc ! \
-  video/x-raw,width=$WIDTH,height=$HEIGHT,framerate=$FRAMERATE,format=NV12,interlace-mode=progressive ! \
+  video/x-raw,width=$WIDTH,height=$HEIGHT,framerate=$FRAMERATE/1,format=NV12,interlace-mode=progressive ! \
   queue max-size-buffers=5 max-size-time=$CAMBUFFERTIME leaky=downstream ! \
   v4l2h264enc extra-controls="controls,repeat_sequence_header=1,video_bitrate=$BITRATE,h264-i-frame-period=30,h264-b-frame=0" ! \
   'video/x-h264,level=(string)4,profile=(string)high' ! \
