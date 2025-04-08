0252fb9e4da697ac7c260a817d2ba1513395697b
Chris L <stylesuxx@gmail.com>
2025-04-09 01:04:52 +0200

    Improve start of control

--- build/build-rc-client.sh
+++ build/build-rc-client.sh
@@ -23,21 +23,22 @@ mkdir -p "${BASE_PATH}"
 mkdir -p "${SERVER_BASE_PATH}"
 mkdir -p "${SERVER_LIB_PATH}"
 mkdir -p "${GST_BASE_PATH}"
+mkdir -p "${PYTHON_LIB_PATH}"
 
 # Move files into place
 cd "${PWD}/.."
 cp -r "./web-server/." "${SERVER_BASE_PATH}"
 cp "./bash/send_cam.sh" "${GST_BASE_PATH}"
+cp -r "./src/rpi_4g_streamer" ${PYTHON_LIB_PATH}
+
+# Delete cache dirs
+find "${DEST_DIR}" -type d -name '__pycache__' -exec rm -r {} +
 
 # Fetch static files for the web server
 curl -o "${SERVER_LIB_PATH}/jsoneditor.min.js" "https://raw.githubusercontent.com/jdorn/json-editor/master/dist/jsoneditor.min.js"
 curl -o "${SERVER_LIB_PATH}/jquery.min.js" "https://code.jquery.com/jquery-3.6.0.min.js"
 curl -o "${SERVER_LIB_PATH}/bootstrap3.min.css" "https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
 
-# Move python libraries
-mkdir -p ${PYTHON_LIB_PATH}
-cd "${PWD}/.."
-cp -r "./src/rpi_4g_streamer" ${PYTHON_LIB_PATH}
 
 # Build the deb package
 gzip -9 -n "${DEST_DIR}/usr/share/doc/${NAME}/changelog"
