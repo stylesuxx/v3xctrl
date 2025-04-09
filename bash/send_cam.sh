4fbe767550aeb73f8677d3ec5d08b948a17075af
Chris L <stylesuxx@gmail.com>
2025-04-09 23:20:45 +0200

    Add config to enable test video source instead of live cam feed

--- bash/send_cam.sh
+++ bash/send_cam.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 if [ $# -lt 2 ]; then
-    echo "Usage: $0 HOST PORT [--width val] [--height val] [--framerate val] [--bitrate val] [--buffertime val] [--sizebuffers val] [--recording-dir val]"
+    echo "Usage: $0 HOST PORT [--width val] [--height val] [--framerate val] [--bitrate val] [--buffertime val] [--sizebuffers val] [--recording-dir val] [--test-pattern]"
     exit 1
 fi
 
@@ -19,7 +19,11 @@ SIZEBUFFERS=5
 BUFFERTIME_UDP=150000000
 SIZEBUFFERS_UDP=5
 
+H264_PROFILE=0
+H264_LEVEL=31
+
 RECORDING_DIR=""
+USE_TEST_PATTERN=0
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -30,6 +34,7 @@ while [[ $# -gt 0 ]]; do
     --buffertime) BUFFERTIME="$2"; shift 2 ;;
     --sizebuffers) SIZEBUFFERS="$2"; shift 2 ;;
     --recording-dir) RECORDING_DIR="$2"; shift 2 ;;
+    --test-pattern) USE_TEST_PATTERN=1; shift ;;
     *)
       echo "Unknown option: $1"
       exit 1
@@ -47,7 +52,29 @@ if [ -n "$RECORDING_DIR" ]; then
   TEE_BRANCH="t. ! queue ! h264parse ! mpegtsmux ! filesink location=$FILENAME"
 fi
 
-gst-launch-1.0 -v libcamerasrc ! \
+SOURCE_BRANCH="libcamerasrc"
+if [ "$USE_TEST_PATTERN" -eq 1 ]; then
+  SOURCE_BRANCH="videotestsrc is-live=true pattern=smpte"
+fi
+
+# Encoder:
+# - I frames every 30 frames (once per second)
+# - IDR frame every 30 frames - this basically means every I frame is and IDR
+#   frame, meaning that following frames can only reference this I frame, n
+#   previous I frame.
+# - B frames are predictive, since we do not know the future, we can explicitly
+#   disable them
+# - Profile: 0 (baseline) no B-frames no CABAC. Profile 4 has better compression
+#   but also intorudces some additional latency (up to 20ms per frame) for a
+#   bitrate reduction of about 15%.
+# - Level: 31 (high) 1280x720, 30FPS, 14Mbps max
+#
+# Test Pattern:
+# smpte can easily be generate live, other patterns not so much, would probably
+# make sense to pre render video with different test patterns. But for a
+# continous datastream smpte is fine anyway (just make sure that the CPU - only
+# one core is used by videotestsrc - is not maxed out.)
+gst-launch-1.0 -v $SOURCE_BRANCH ! \
   "video/x-raw,width=$WIDTH,height=$HEIGHT,framerate=$FRAMERATE/1,format=NV12,interlace-mode=progressive" ! \
   queue \
     max-size-buffers=$SIZEBUFFERS \
@@ -55,11 +82,13 @@ gst-launch-1.0 -v libcamerasrc ! \
     leaky=downstream ! \
   v4l2h264enc extra-controls="controls,\
     repeat_sequence_header=1,\
-    video_bitrate=$BITRATE,\
-    bitrate_mode=constant,\
-    h264_i_frame_period=30,\
-    h264_idr_interval=30,\
-    h264_b_frame=0" ! \
+    video_bitrate=${BITRATE},\
+    bitrate_mode=${BITRATE_MODE},\
+    video_gop_size=${FRAMERATE},\
+    h264_i_frame_period=${FRAMERATE},\
+    video_b_frames=0,\
+    h264_profile=${H264_PROFILE},\
+    h264_level=${H264_LEVEL}" ! \
   "video/x-h264,level=(string)4,profile=(string)high,stream-format=(string)byte-stream" ! \
   tee name=t \
     t. ! queue \
