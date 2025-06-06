9f46100bcb090cd9cb2d9d0e505b99a34e256d1a
Noctaro <nocturn@quadrocopter.at>
2025-06-06 22:22:06 +0200

    Changing shutdown message

--- web-server/static/js/handlers.js
+++ web-server/static/js/handlers.js
@@ -44,7 +44,8 @@ const registerClickHandlers = (editor) => {
     `;
 
     showModal("Shutting down", html, 30, () => {
-    $('#modal-body').html('<p><strong>Shutdown complete.</strong></p>');
+    $('#modal-title').text("Shutdown complete");
+    $('#modal-body').html('<p><strong>It is safe to turn off now.</strong></p>');
     });
 
     $.post('/shutdown').fail(() => {
