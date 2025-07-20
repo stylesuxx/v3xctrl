93470edf04bfc0746d799a57e1cb344d0a53c640
Chris L <stylesuxx@gmail.com>
2025-07-20 23:36:26 +0200

    Refactored handlers into Tabs class where it makes sense #117

--- web-server/static/js/Tabs.js
+++ web-server/static/js/Tabs.js
@@ -1,201 +1,59 @@
 class Tabs {
-  constructor(container) {
+  constructor(container, editor) {
     this.hash = window.location.hash;
-    this.$container = $(container);
+
+    this.container = container;
+    this.editor = editor;
+
+    this.$container = $(this.container);
     this.$menu = this.$container.find('ul.nav');
     this.$panes = this.$container.find('.tab-pane');
+
+    this.tabs = {
+      "calibration": this.$panes.filter('#calibration'),
+      "dmesg": this.$panes.filter('#dmesg'),
+      "editor": this.$panes.filter('#editor'),
+      "modem": this.$panes.filter('#modem'),
+      "services": this.$panes.filter('#services'),
+    };
 
     this.registerTabHandler();
     this.activateTabFromHash();
   }
 
   registerTabHandler() {
+    const that = this;
+
     this.$menu.find('li a').on('click', (e) => {
       e.preventDefault();
 
       const $target = $(e.currentTarget)
       const target = $target.attr('href');
 
-      this.$menu.find('li').removeClass('active');
+      that.$menu.find('li').removeClass('active');
       $target.parent().addClass('active');
 
-      this.$panes.hide();
-      this.$container.find(target).show();
+      that.$panes.hide();
+      that.$container.find(target).show();
 
       history.replaceState(null, null, target);
 
-      // Load tab content
+      // Render tab content
       switch(target) {
         case '#services': {
-          API.getServices().then((services) => {
-            const $container = $('#services .container');
-            $container.empty();
-
-            if (services && services.length > 0) {
-              const $table = $('<table />', {
-                class: 'table table-striped',
-              });
-              const $tableHead = $('<thead><tr><th>Service</th><th>Type</th><th>Status</th><th></th><th></th></tr></thead>');
-              const $tbody = $('<tbody />');
-
-              $table.append($tableHead);
-              $table.append($tbody);
-              $container.append($table);
-
-              services.forEach((service) => {
-                var $row = $('<tr />', {
-                  'data-name': service.name,
-                });
-
-                let stateLabel = 'Failed';
-                let success = false;
-
-                switch(service.type) {
-                  case 'oneshot': {
-                    if(
-                      service.result === 'success' &&
-                      ['active', 'activating'].includes(service.state)
-                    ) {
-                      success = true;
-
-                      stateLabel = "Ran";
-                      if(service.state == 'activating') {
-                        stateLabel = "Running";
-                      }
-                    }
-                  } break;
-
-                  case 'forking': {
-                    stateLabel  = "Failed";
-                    if(
-                      service.result === 'success' &&
-                      service.state === 'active'
-                    ) {
-                      success = true;
-                      stateLabel = "Active";
-                    }
-                  } break;
-
-                  case 'simple': {
-                    stateLabel  = "Inactive";
-                    if(
-                      service.result === 'success' &&
-                      service.state === 'active'
-                    ) {
-                      success = true;
-                      stateLabel = "Active";
-                    }
-                  } break;
-                }
-
-                const statusClass = success ? 'text-success' : 'text-danger';
-                const $line = $('<p />', {'data-name': service.name})
-                $line.append(`<strong>${service.name} (${service.type})</strong>: <span class="${statusClass}">${service.state}</span>`)
-
-                $row.append(`<td><strong>${service.name}</strong></td>`);
-                $row.append(`<td>${service.type}</td>`);
-                $row.append(`<td><span class="${statusClass}">${stateLabel}</span></td>`);
-
-                const $button = $('<td />');
-                $row.append($button);
-
-                if(
-                  service.type === 'simple' ||
-                  service.type === 'forking'
-                ) {
-                  if(success) {
-                    $button.append('<button class="service-stop btn btn-primary">Stop</button>');
-                  } else {
-                    $button.append('<button class="service-start btn btn-primary">Start</button>');
-                  }
-                }
-
-                if(service.type === 'oneshot') {
-                  if(
-                    service.result === 'success' &&
-                    ['active', 'activating'].includes(service.state)
-                  ) {
-                    if(service.state == 'activating') {
-                      $button.append('<button class="service-stop btn btn-primary">Stop</button>');
-                    } else {
-                      $button.append('<button class="service-restart btn btn-primary">Restart</button>');
-                    }
-                  } else {
-                    $button.append('<button class="service-restart btn btn-primary">Restart</button>');
-                  }
-                }
-
-                $row.append('<td><button class="service-log btn btn-secondary">Show logs</button></td>');
-
-                $tbody.append($row);
-              });
-
-              const getName = function(e) {
-                e.preventDefault();
-
-                const $target = $(e.currentTarget);
-                const name = $target.closest('tr').data('name');
-
-                return name;
-              }
-
-              $('button.service-start').on('click', (e) => {
-                const name = getName(e);
-                API.startService(name).then(() => {
-                  this.$menu.find(`a[href="#services"]`).click();
-                });
-              });
-
-              $('button.service-stop').on('click', (e) => {
-                const name = getName(e);
-                API.stopService(name).then(() => {
-                  this.$menu.find(`a[href="#services"]`).click();
-                });
-              });
-
-              $('button.service-restart').on('click', (e) => {
-                const name = getName(e);
-                API.restartService(name).then(() => {
-                  this.$menu.find(`a[href="#services"]`).click();
-                });
-              });
-
-              $('button.service-log').on('click', (e) => {
-                const name = getName(e);
-                API.getServiceLog(name).then((log) => {
-                  const $log = $("#service-log")
-                  $log.val(log);
-                  $log.removeClass("hidden");
-                });
-              });
-            } else {
-              $container.text('No service info available.');
-            }
-
-          });
+          that.renderServices();
         } break;
 
         case '#dmesg': {
-          API.getDmesg().then((log) => {
-            this.$panes.filter('#dmesg').find('textarea').val(log);
-          });
+          that.renderDmesg();
         } break;
 
         case '#modem': {
-          Tabs.renderModemInfo();
+          that.renderModemInfo();
         } break;
 
         case '#calibration': {
-          API.getServices().then((services) => {
-            services.forEach((service) => {
-              if(service.name === 'v3xctrl-control') {
-                if(!service.active_state) {
-                  $('.service-warning').addClass('hidden');
-                  $('.calibration-content').removeClass('hidden');
-                }
-              }
-            });
-          });
+          that.renderCalibration();
         } break;
 
         default: {
@@ -205,13 +63,300 @@ class Tabs {
     });
 
     window.addEventListener("hashchange", () => {
-      this.hash = window.location.hash;
-      this.activateTabFromHash();
+      that.hash = window.location.hash;
+      that.activateTabFromHash();
+    });
+
+    /**
+     * Click handlers for static UI elements, related to the tabs
+     */
+
+    // Modem
+    this.tabs.modem.find('button.reset').on('click', (e) => {
+      const modal = new Modal('Resetting modem', '<p>This will take a couple seconds...</p>');
+      modal.show();
+
+      API.resetModem()
+        .then(() => {
+          that.renderModemInfo()
+            .catch(() => {
+              console.warn('Failed to fetch modem info.');
+            })
+            .finally(() => {
+              modal.remove();
+            });
+        });
+    });
+
+    // dmesg
+    this.tabs.dmesg.find('button.refresh').on('click', function(e) {
+      API.getDmesg();
+    });
+
+    // calibration
+    this.tabs.calibration.find('button.save-steering-calibration').on('click', (e) => {
+      const $calibration = that.tabs.calibration;
+
+      const min = parseInt($calibration.find('.steering.min input').val());
+      const max = parseInt($calibration.find('.steering.max input').val());
+      const trim = parseInt($calibration.find('.steering.trim input').val());
+
+      const values = that.editor.getValue();
+      values.controls.steering.min = min;
+      values.controls.steering.max = max;
+      values.controls.steering.trim = trim;
+
+      that.editor.setValue(values);
+      that.editor.save();
+    });
+
+    this.tabs.calibration.find('button.save-throttle-calibration').on('click', (e) => {
+      const $calibration = that.tabs.calibration;
+
+      const min = parseInt($calibration.find('.throttle.min input').val());
+      const max = parseInt($calibration.find('.throttle.max input').val());
+      const idle = parseInt($calibration.find('.throttle.idle input').val());
+
+      const values = that.editor.getValue();
+      values.controls.throttle.min = min;
+      values.controls.throttle.max = max;
+      values.controls.throttle.idle = idle;
+
+      that.editor.setValue(values);
+      that.editor.save();
+    });
+
+    this.tabs.calibration.find('form.steering button').on('click', (e) => {
+      e.preventDefault();
+
+      const $this = $(e.target);
+      const $form = $this.closest('form');
+      const $input = $form.find('input');
+      let value = parseInt($input.val());
+
+      if($this.hasClass('increase')) {
+        value += 10;
+      }
+
+      if($this.hasClass('decrease')) {
+        value -= 10;
+      }
+      $input.val(value);
+
+      if($form.hasClass('trim')) {
+        const min = parseInt($('.steering.min input').val());
+        const max = parseInt($('.steering.max input').val());
+
+        const base = min + ((max - min) / 2);
+        value = base + value;
+      }
+
+      const values = that.editor.getValue();
+      const gpio = parseInt(values.controls.gpio.steering);
+      API.setPwm(gpio, value);
+    });
+
+    this.tabs.calibration.find('form.throttle button').on('click', (e) => {
+      e.preventDefault();
+
+      const $this = $(e.target);
+      const $form = $this.closest('form');
+      const $input = $form.find('input');
+      let value = parseInt($input.val());
+
+      if($this.hasClass('increase')) {
+        value += 10;
+      }
+
+      if($this.hasClass('decrease')) {
+        value -= 10;
+      }
+      $input.val(value);
+
+      const values = that.editor.getValue();
+      const gpio = parseInt(values.controls.gpio.throttle);
+      API.setPwm(gpio, value);
     });
   }
 
-  static async renderModemInfo() {
+  async renderServices() {
+    API.getServices().then((services) => {
+      const $container = this.tabs.services.find('.container');
+      $container.empty();
+
+      if (services && services.length > 0) {
+        const $table = $('<table />', {
+          class: 'table table-striped',
+        });
+        const $tableHead = $('<thead><tr><th>Service</th><th>Type</th><th>Status</th><th></th><th></th></tr></thead>');
+        const $tbody = $('<tbody />');
+
+        $table.append($tableHead);
+        $table.append($tbody);
+        $container.append($table);
+
+        services.forEach((service) => {
+          var $row = $('<tr />', {
+            'data-name': service.name,
+          });
+
+          let stateLabel = 'Failed';
+          let success = false;
+
+          switch(service.type) {
+            case 'oneshot': {
+              if(
+                service.result === 'success' &&
+                ['active', 'activating'].includes(service.state)
+              ) {
+                success = true;
+
+                stateLabel = "Ran";
+                if(service.state == 'activating') {
+                  stateLabel = "Running";
+                }
+              }
+            } break;
+
+            case 'forking': {
+              stateLabel  = "Failed";
+              if(
+                service.result === 'success' &&
+                service.state === 'active'
+              ) {
+                success = true;
+                stateLabel = "Active";
+              }
+            } break;
+
+            case 'simple': {
+              stateLabel  = "Inactive";
+              if(
+                service.result === 'success' &&
+                service.state === 'active'
+              ) {
+                success = true;
+                stateLabel = "Active";
+              }
+            } break;
+          }
+
+          const statusClass = success ? 'text-success' : 'text-danger';
+          const $line = $('<p />', {'data-name': service.name})
+          $line.append(`<strong>${service.name} (${service.type})</strong>: <span class="${statusClass}">${service.state}</span>`)
+
+          $row.append(`<td><strong>${service.name}</strong></td>`);
+          $row.append(`<td>${service.type}</td>`);
+          $row.append(`<td><span class="${statusClass}">${stateLabel}</span></td>`);
+
+          const $button = $('<td />');
+          $row.append($button);
+
+          if(
+            service.type === 'simple' ||
+            service.type === 'forking'
+          ) {
+            if(success) {
+              $button.append('<button class="service-stop btn btn-primary">Stop</button>');
+            } else {
+              $button.append('<button class="service-start btn btn-primary">Start</button>');
+            }
+          }
+
+          if(service.type === 'oneshot') {
+            if(
+              service.result === 'success' &&
+              ['active', 'activating'].includes(service.state)
+            ) {
+              if(service.state == 'activating') {
+                $button.append('<button class="service-stop btn btn-primary">Stop</button>');
+              } else {
+                $button.append('<button class="service-restart btn btn-primary">Restart</button>');
+              }
+            } else {
+              $button.append('<button class="service-restart btn btn-primary">Restart</button>');
+            }
+          }
+
+          $row.append('<td><button class="service-log btn btn-secondary">Show logs</button></td>');
+
+          $tbody.append($row);
+        });
+
+        const getName = function(e) {
+          e.preventDefault();
+
+          const $target = $(e.currentTarget);
+          const name = $target.closest('tr').data('name');
+
+          return name;
+        }
+
+        $('button.service-start').on('click', (e) => {
+          const name = getName(e);
+          API.startService(name).then(() => {
+            this.$menu.find(`a[href="#services"]`).click();
+          });
+        });
+
+        $('button.service-stop').on('click', (e) => {
+          const name = getName(e);
+          API.stopService(name).then(() => {
+            this.$menu.find(`a[href="#services"]`).click();
+          });
+        });
+
+        $('button.service-restart').on('click', (e) => {
+          const name = getName(e);
+          API.restartService(name).then(() => {
+            this.$menu.find(`a[href="#services"]`).click();
+          });
+        });
+
+        $('button.service-log').on('click', (e) => {
+          const name = getName(e);
+          API.getServiceLog(name).then((log) => {
+            const $log = $("#service-log")
+            $log.val(log);
+            $log.removeClass("hidden");
+          });
+        });
+      } else {
+        $container.text('No service info available.');
+      }
+    });
+  }
+
+  async renderDmesg() {
+    const $content = this.tabs.dmesg;
+
+    API.getDmesg().then((log) => {
+      $content.find('textarea').val(log);
+    });
+  }
+
+  async renderCalibration() {
+    const $warning = this.tabs.calibration.find('.service-warning');
+    const $content = this.tabs.calibration.find('.calibration-content');
+
+    API.getServices().then((services) => {
+      services.forEach((service) => {
+        if(service.name === 'v3xctrl-control') {
+          if(!service.active_state) {
+            $warning.addClass('hidden');
+            $content.removeClass('hidden');
+          } else {
+            $warning.removeClass('hidden');
+            $content.addClass('hidden');
+          }
+        }
+      });
+    });
+  }
+
+  async renderModemInfo() {
     const info = await API.getModemInfo();
+    const $content = this.tabs.modem.find('p');
 
     const $table = $("<table />", {
       class: "table"
@@ -254,7 +399,7 @@ class Tabs {
       $tbody.append($row);
     }
 
-    $("#modem p").html($table);
+    $content.html($table);
   }
 
   activateTabFromHash() {
--- web-server/static/js/handlers.js
+++ web-server/static/js/handlers.js
@@ -1,8 +1,8 @@
 /**
- * Click handlers for static UI elements
+ * Click handlers for static UI elements (not directly related to the tabs)
  */
 
-const registerClickHandlers = (editor) => {
+const registerClickHandlers = () => {
   $('a.reboot').on('click', function(e) {
     e.preventDefault();
     $(this).prop('disabled', true);
@@ -50,108 +50,5 @@ const registerClickHandlers = (editor) => {
     } catch(err) {
       console.warn(err);
     }
-  });
-
-  $('button.save-steering-calibration').on('click', function(e) {
-    const $calibration = $('#calibration');
-    const min = parseInt($calibration.find('.steering.min input').val());
-    const max = parseInt($calibration.find('.steering.max input').val());
-    const trim = parseInt($calibration.find('.steering.trim input').val());
-
-    const values = editor.getValue();
-    values.controls.steering.min = min;
-    values.controls.steering.max = max;
-    values.controls.steering.trim = trim;
-
-    editor.setValue(values);
-    editor.save();
-  });
-
-  $('button.save-throttle-calibration').on('click', function(e) {
-    const $calibration = $('#calibration');
-    const min = parseInt($calibration.find('.throttle.min input').val());
-    const max = parseInt($calibration.find('.throttle.max input').val());
-    const idle = parseInt($calibration.find('.throttle.idle input').val());
-
-    const values = editor.getValue();
-    values.controls.throttle.min = min;
-    values.controls.throttle.max = max;
-    values.controls.throttle.idle = idle;
-
-    editor.setValue(values);
-    editor.save();
-  });
-
-  $('form.steering button').on('click', function(e) {
-    e.preventDefault();
-
-    const $this = $(this);
-    const $form = $this.closest('form');
-    const $input = $form.find('input');
-    let value = parseInt($input.val());
-
-    if($this.hasClass('increase')) {
-      value += 10;
-    }
-
-    if($this.hasClass('decrease')) {
-      value -= 10;
-    }
-    $input.val(value);
-
-    if($form.hasClass('trim')) {
-      const min = parseInt($('.steering.min input').val());
-      const max = parseInt($('.steering.max input').val());
-
-      const base = min + ((max - min) / 2);
-      value = base + value;
-    }
-
-    const values = editor.getValue();
-    const gpio = parseInt(values.controls.gpio.steering);
-    API.setPwm(gpio, value);
-  });
-
-  $('form.throttle button').on('click', function(e) {
-    e.preventDefault();
-
-    const $this = $(this);
-    const $form = $this.closest('form');
-    const $input = $form.find('input');
-    let value = parseInt($input.val());
-
-    if($this.hasClass('increase')) {
-      value += 10;
-    }
-
-    if($this.hasClass('decrease')) {
-      value -= 10;
-    }
-    $input.val(value);
-
-    const values = editor.getValue();
-    const gpio = parseInt(values.controls.gpio.throttle);
-    API.setPwm(gpio, value);
-  });
-
-  $('button.dmesg-refresh').on('click', function(e) {
-    API.getDmesg();
-  });
-
-  $('#modem button.reset').on('click', (e) => {
-    const modal = new Modal('Resetting modem', '<p>This will take a couple seconds...</p>');
-    modal.show();
-
-    API.resetModem()
-      .then(() => {
-        Tabs.renderModemInfo()
-          .catch(() => {
-            console.warn('Failed to fetch modem info.');
-          })
-          .finally(() => {
-            modal.hide();
-            modal.remove();
-          });
-      });
   });
 };
