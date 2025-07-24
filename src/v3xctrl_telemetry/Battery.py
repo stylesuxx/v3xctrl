27e5b28eded376fdcb74c9b843ff3346f1fef05e
Chris L <stylesuxx@gmail.com>
2025-07-24 11:50:46 +0200

    Send voltage, average voltage, percentage and warning flag as telemetry #52

--- src/v3xctrl_telemetry/Battery.py
+++ src/v3xctrl_telemetry/Battery.py
@@ -1,6 +1,7 @@
 from v3xctrl_telemetry import INA
 from v3xctrl_helper import clamp
 
+
 class Battery:
     def __init__(
         self,
@@ -16,20 +17,24 @@ class Battery:
         self.warn_cell_voltage = warn_cell_voltage
 
         self.voltage = 0
+        self.average_voltage = 0
         self.warning = False
         self.percentage = 100
         self.cell_count = self._guess_cell_count()
 
     def _guess_cell_count(self) -> int:
-        # Guess cell count based on average cell voltage
+        """
+        Guess cell count based on average cell voltage - minimum 1 to prevent
+        division by zero
+        """
         voltage = self._sensor.get_bus_voltage()
         average_cell_voltage = (self.min_cell_voltage + self.max_cell_voltage) / 2
 
-        return round(voltage / average_cell_voltage)
+        return max(round(voltage / average_cell_voltage), 1)
 
     def update(self) -> None:
-        # Returns the bus voltage in mV
         self.voltage = self._sensor.get_bus_voltage()
+        self.average_voltage = round(self.voltage / self.cell_count)
 
         # Calculate percentage
         min_voltage = self.min_cell_voltage * self.cell_count
