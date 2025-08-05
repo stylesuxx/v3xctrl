3c3ff0c429d2ce3e8becbff886f23438636b5940
Chris L <stylesuxx@gmail.com>
2025-08-06 01:13:20 +0200

    Intermitten commit: pass bands to be reset if specific model is chosen

--- src/v3xctrl_helper/modem/reset.py
+++ src/v3xctrl_helper/modem/reset.py
@@ -1,11 +1,18 @@
 import argparse
 import logging
 from atlib import AIR780EU
+import time
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Reset modem to auto connect and a single empty IPv4 APN")
     parser.add_argument("device_path", help="Path to the modem (e.g. /dev/ttyACM0)")
+    parser.add_argument(
+        "--bands",
+        help="Comma-separated list of LTE band integers (e.g. 1,3,7,20)",
+        type=lambda s: [int(x) for x in s.split(',')] if s else [],
+        default=[]
+    )
     args = parser.parse_args()
 
     # Connect to modem, turn radio off, set operator to auto connect
@@ -19,12 +26,18 @@ def main() -> None:
         id = context.id
         gsm.delete_context(id)
 
+    if len(args.bands) > 0:
+        gsm.set_allowed_bands(args.bands)
+
     # Reboots the modem, serial connection will be lost, radio will be powered
     # back on during a reboot cycle.
     try:
         gsm.reboot()
     except OSError:
         logging.warning("Lost connection during reboot...")
+
+    # Give the modem some time to reboot
+    time.sleep(5)
 
 
 if __name__ == "__main__":
