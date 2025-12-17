02e79e50f253f26d94019364fa98107f55561c0c
Chris L <stylesuxx@gmail.com>
2025-12-17 18:41:10 +0100

    Add missing dependencies

--- build/chroot/build-debs.sh
+++ build/chroot/build-debs.sh
@@ -14,6 +14,15 @@ while [[ "$#" -gt 0 ]]; do
   shift
 done
 
+# Fix locale only if not already configured
+if ! locale -a | grep -q "^${LOCALE}$"; then
+  echo '[CHROOT] Fixing locale'
+  echo "$LOCALE UTF-8" >> /etc/locale.gen
+  locale-gen
+  echo "LANG=$LOCALE" > /etc/default/locale
+  update-locale LANG=$LOCALE
+fi
+
 # Change to working directory
 cd /src
 
@@ -22,20 +31,32 @@ if [ "$SKIP_DEPS" = false ]; then
   apt update
   apt upgrade -y
   apt install -y \
-    curl build-essential libssl-dev libbz2-dev libsqlite3-dev ca-certificates \
-    liblzma-dev libreadline-dev libffi-dev libgdbm-dev libgdbm-compat-dev \
-    libdb-dev uuid-dev zlib1g-dev libncursesw5-dev tk-dev \
-    libctypes-ocaml-dev libcurses-ocaml-dev dphys-swapfile lintian
+    build-essential \
+    curl \
+    ca-certificates \
+    dphys-swapfile \
+    libbz2-dev \
+    libcairo2-dev \
+    libctypes-ocaml-dev \
+    libcurses-ocaml-dev \
+    libdb-dev \
+    libffi-dev \
+    libgdbm-dev \
+    libgdbm-compat-dev \
+    libgirepository1.0-dev \
+    liblzma-dev \
+    libncursesw5-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    lintian \
+    tk-dev \
+    uuid-dev \
+    zlib1g-dev
 fi
 
 # Build v3xctrl-python only if it does not exist yet
 if ! dpkg -s v3xctrl-python >/dev/null 2>&1; then
-  echo '[CHROOT] Fixing locale'
-  echo "$LOCALE UTF-8" >> /etc/locale.gen
-  locale-gen
-  echo "LANG=$LOCALE" > /etc/default/locale
-  update-locale LANG=$LOCALE
-
   echo '[CHROOT] Building Python'
   ./build/build-python.sh /src
   apt install -y ./build/tmp/v3xctrl-python.deb
