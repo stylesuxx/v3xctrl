ec566991d71660b4969719f77964557df9fcc7b3
Chris L <stylesuxx@gmail.com>
2026-02-10 23:10:08 +0100

    chore(#483): By default add the custom repo, when local flag is used, use local files

--- build/chroot/customize-image.sh
+++ build/chroot/customize-image.sh
@@ -4,6 +4,15 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
+# Parse flags
+USE_LOCAL_DEBS=false
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --local|-l) USE_LOCAL_DEBS=true ;;
+  esac
+  shift
+done
+
 USER="v3xctrl"
 NAME="v3xctrl"
 LOCALE="en_US.UTF-8"
@@ -11,6 +20,12 @@ LOCALE="en_US.UTF-8"
 echo '[CHROOT] Fetching signed regulatory domain files'
 curl -L https://kernel.googlesource.com/pub/scm/linux/kernel/git/sforshee/wireless-regdb/+/refs/heads/master/regulatory.db\?format=TEXT | base64 -d | tee /lib/firmware/regulatory.db > /dev/null
 curl -L https://kernel.googlesource.com/pub/scm/linux/kernel/git/sforshee/wireless-regdb/+/refs/heads/master/regulatory.db.p7s\?format=TEXT | base64 -d | tee /lib/firmware/regulatory.db.p7s > /dev/null
+
+if [ "$USE_LOCAL_DEBS" = false ]; then
+  echo '[CHROOT] Adding V3XCTRL repository...'
+  curl -fsSL https://repo.v3xctrl.com/public.key | gpg --dearmor -o /usr/share/keyrings/v3xctrl.gpg
+  echo "deb [signed-by=/usr/share/keyrings/v3xctrl.gpg arch=arm64] https://repo.v3xctrl.com trixie main" > /etc/apt/sources.list.d/v3xctrl.list
+fi
 
 echo '[CHROOT] Updating system...'
 apt-get update
@@ -29,11 +44,15 @@ echo '[CHROOT] Fixing locale'
 locale-gen $LOCALE
 update-locale LANG="$LOCALE"
 
-echo '[CHROOT] Installing dependencies...'
-apt-get install -y /tmp/*.deb
+echo '[CHROOT] Installing V3XCTRL packages...'
+if [ "$USE_LOCAL_DEBS" = true ]; then
+  apt-get install -y /tmp/*.deb
+  rm -f /tmp/*.deb
+else
+  apt-get install -y v3xctrl
+fi
 
 echo '[CHROOT] Removing bloat...'
-rm -f /tmp/*.deb
 apt-get autoremove -y
 apt-get clean
 
