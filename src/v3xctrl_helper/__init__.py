f610249e7dd30b1f17a72e3ceffcfd0170aef2f5
Chris L <stylesuxx@gmail.com>
2026-02-25 13:37:41 +0100

    Add functionality to fetch time and offset

--- src/v3xctrl_helper/__init__.py
+++ src/v3xctrl_helper/__init__.py
@@ -9,13 +9,21 @@ from v3xctrl_helper.helper import (
     color_to_hex,
     is_int,
 )
+from v3xctrl_helper.ntp import (
+  NTPClock,
+  get_ntp_offset_chrony,
+  get_ntp_offset_ntplib,
+)
 
 __all__ = [
     "Address",
     "MessageFromAddress",
+    "NTPClock",
     "PeerAddresses",
     "apply_expo",
     "clamp",
     "color_to_hex",
+    "get_ntp_offset_chrony",
+    "get_ntp_offset_ntplib",
     "is_int",
 ]
