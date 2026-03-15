f799e5be467303a77ce8dd4f5d16d617de808347
Chris L <stylesuxx@gmail.com>
2026-03-15 02:22:15 +0100

    Fix cyclic dependency

--- src/v3xctrl_helper/exceptions.py
+++ src/v3xctrl_helper/exceptions.py
@@ -1,4 +1,9 @@
-from v3xctrl_control.message import PeerInfo
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from v3xctrl_control.message import PeerInfo
 
 
 class UnauthorizedError(Exception):
