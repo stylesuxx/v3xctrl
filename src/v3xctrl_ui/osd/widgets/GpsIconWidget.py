15a1445a4fbdf4718527ec35ae608c2209bdebca
Noctaro <noccopter@gmail.com>
2026-03-19 05:59:48 +0100

    adjust gps widget

--- src/v3xctrl_ui/osd/widgets/GpsIconWidget.py
+++ src/v3xctrl_ui/osd/widgets/GpsIconWidget.py
@@ -1,18 +1,23 @@
-from pygame import Surface
+from pygame import SRCALPHA, Surface
 
 from v3xctrl_ui.osd.widgets.Widget import Widget
 from v3xctrl_ui.utils.colors import GREEN, GREY, ORANGE, RED
 from v3xctrl_ui.utils.helpers import get_icon
 
+_BACKGROUND_ALPHA = 180
+
 
 class GpsIconWidget(Widget):
-    _ICON_SIZE = 70
+    _ICON_SIZE = 46  # matches BatteryIconWidget height (int(70 / 3 * 2))
 
     def __init__(self, position: tuple[int, int], width: int) -> None:
         self.position = position
         self.width = width
         self.height = self._ICON_SIZE
         self._x_offset = width - self._ICON_SIZE  # right-align icon within panel width
+
+        self._bg_surface = Surface((width, self._ICON_SIZE), SRCALPHA)
+        self._bg_surface.fill((*GREY, _BACKGROUND_ALPHA))
 
         self.icon_no_hardware = get_icon("satellite_alt", size=self._ICON_SIZE, color=GREY)
         self.icon_no_fix = get_icon("satellite_alt", size=self._ICON_SIZE, color=RED)
@@ -29,4 +34,5 @@ class GpsIconWidget(Widget):
         else:
             icon = self.icon_full_fix
 
+        screen.blit(self._bg_surface, self.position)
         screen.blit(icon, (self.position[0] + self._x_offset, self.position[1]))
